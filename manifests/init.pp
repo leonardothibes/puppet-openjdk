967293c63eb5139d292fcddd02e3e0ccbfb305b7
Leonardo Thibes <eu@leonardothibes.com>
2014-04-09 11:28:18 -0300

    implementando a classe de instalacao

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,16 @@
-# == Class: openjdk
-#
-# Full description of class openjdk here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
-# === Examples
-#
-#  class { openjdk:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2014 Your name here, unless otherwise noted.
-#
-class openjdk {
-
-
+class openjdk
+{
+    if $osfamily != 'Debian' {
+        fail("Unsupported platform: ${osfamily}/${operatingsystem}")
+    }
+    package {'openjdk::package':
+        ensure => present,
+        name   => $openjdk::params::package,
+    }
+    file {'openjdk::link':
+        ensure  => link,
+        path    => $openjdk::params::java_path,
+        target  => $openjdk::params::java_target,
+        require => Package['openjdk::package'],
+    }
 }
